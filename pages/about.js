b440444c6a582d01c62e1a2789fb5458d54324ec
davidbudzynski <david.budzynsky@gmail.com>
2020-04-28 21:14:31 +0100

    added basic pages and navbar

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar"
+import Layout from "../components/Layout"
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>BitzPrice is an application where you can check the most up-to-date prices of Bitcoin, so you can find out how much money you lost where the prices drop every so often!</p>
+    </div>
+  </Layout>
+)
+
+export default About 
